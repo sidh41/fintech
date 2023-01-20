c57d72267b1b9c246c9b01827703670027aaa540
Siddhesh Shedge <32062337+sidh41@users.noreply.github.com>
2023-01-20 17:24:28 +0530

    Uploaded files

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,15 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class Tab1Page {
+
+  constructor() {}
+
+}
